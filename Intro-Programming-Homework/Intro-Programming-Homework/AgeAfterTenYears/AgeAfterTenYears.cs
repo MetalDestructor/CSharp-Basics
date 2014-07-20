dfcc9e2a184270a5895004da883b1f1cf672f1ec
MetalDestructor <rapid3fighter@hotmail.com>
2014-07-20 14:46:59 +0300

    Introduction Programming Homeworks

---
+++ Intro-Programming-Homework/Intro-Programming-Homework/AgeAfterTenYears/AgeAfterTenYears.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class AgeAfterTenYears
+{
+    static void Main()
+    {
+        Console.Write("Your current Age : ");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Your current age is : " + age);
+        Console.WriteLine("After 10 years you will be : " + (age+10));
+    }
+}
---
+++ Intro-Programming-Homework/Intro-Programming-Homework/Print-My-Name/PrintMyName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class PrintMyName
+{
+    static void Main()
+    {
+        string myName = "Pesho Geshov";
+
+        Console.WriteLine("My name is {0}",myName);
+    }
+}
+
