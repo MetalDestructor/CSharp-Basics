426e823d11d9296656a06a7088210a87f469dcd9
MetalDestructor <rapid3fighter@hotmail.com>
2015-08-25 02:09:28 +0300

    All of the Console Input/Output Homework // w/o 11 & 12

---
+++ Console-Input-Output-Homework/Problem10/FibonacciNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem10
+{
+    class FibonacciNumbers
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = b + temp;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write($"{Fibonacci(i)} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
