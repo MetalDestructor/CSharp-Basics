e59879f7b53a8ee47af903e631365e856a3d5e76
MetalDestructor <rapid3fighter@hotmail.com>
2015-12-25 22:02:55 +0200

    Added all except advanced

---
+++ Loops-Homework/Problem13/BinaryToDecimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem13
+{
+    class BinaryToDecimal
+    {
+        static void Main(string[] args)
+        {
+            string binaryNum = Console.ReadLine();
+            long result = 0;
+            int power = binaryNum.Length - 1;
+            foreach (char num in binaryNum.ToCharArray())
+            {
+                int iNum = (int)char.GetNumericValue(num);
+                result += iNum * (long)Math.Pow(2, power);
+                --power;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Loops-Homework/Problem15/HexadecimalToDecimal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem15
+{
+    class HexadecimalToDecimal
+    {
+        static void Main(string[] args)
+        {
+            string hexNum = Console.ReadLine();
+            long result = 0;
+            int power = hexNum.Length - 1;
+            int iNum = 0;
+            foreach (char num in hexNum.ToCharArray())
+            {
+                switch (num)
+                {
+                    case 'A':
+                        iNum = 10;
+                        result += iNum * (long)Math.Pow(16, power);
+                        --power;
+                        break;
+
+                    case 'B':
+                        iNum = 11;
+                        result += iNum * (long)Math.Pow(16, power);
+                        --power;
+                        break;
+
+                    case 'C':
+                        iNum = 12;
+                        result += iNum * (long)Math.Pow(16, power);
+                        --power;
+                        break;
+
+                    case 'D':
+                        iNum = 13;
+                        result += iNum * (long)Math.Pow(16, power);
+                        --power;
+                        break;
+
+                    case 'E':
+                        iNum = 14;
+                        result += iNum * (long)Math.Pow(16, power);
+                        --power;
+                        break;
+                    case 'F':
+                        iNum = 15;
+                        result += iNum * (long)Math.Pow(16, power);
+                        --power;
+                        break;
+
+                    default:
+                        iNum = (int)char.GetNumericValue(num);
+                        result += iNum * (long)Math.Pow(16, power);
+                        --power;
+                        break;
+                }
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
